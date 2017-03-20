0e2b0066f628d1ac09eff05d7fff48b0d4a6b429
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-03-20 04:08:10 +0300

    tab js for main section added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -459,3 +459,24 @@ function initMap() {
     map.mapTypes.set('map_style', styledMap);
     map.setMapTypeId('map_style');
 }
+
+
+function openTab(evt, tabName) {
+ 
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
