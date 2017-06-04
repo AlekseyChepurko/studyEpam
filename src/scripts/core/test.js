801ecdcf46c16181ae0b3574afff9e9aee4788fd
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-06-04 03:13:43 +0300

    Color customization added

--- src/scripts/core/test.js
+++ src/scripts/core/test.js
@@ -4,6 +4,6 @@
 
 export default function test(testName,res,expected){
     if (res === expected)
-        return console.log(`OK! - ${testName}`);
-    return console.log(`Failure - ${testName}: Expected ${expected} given ${res}`);
+        return console.log(`%c OK! - ${testName}`, 'color: green;');
+    return console.log(`%c Failure - ${testName}: Expected ${expected} given ${res}`, 'color: red; font-weight: bold;');
 }
