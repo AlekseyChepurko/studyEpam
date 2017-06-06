41024378e4b7bc039b86a0b98f3cd908adcb915e
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-06-06 03:35:31 +0300

    Common Async class deleted - since now everything is separate module

--- src/scripts/core/Async.js
+++ src/scripts/core/Async.js
@@ -1,16 +1,25 @@
 /**
  * Created by Алексей on 03.06.2017.
  */
+//
+// export default class Async {
+//
+// }
 
-export default class Async {
-    static getJson(url,callback=undefined){
-        return fetch(url).then((response)=> response.json()).then((res)=>{
-            return callback === undefined
-                ? res
-                : typeof callback === "function"
-                    ? callback(res)
-                    : ()=>{throw "Callback must be a function"}
-                ;
-        });
-    }
+
+/**
+ * Get JSON from url using fetch. After fetch resolving calls callback
+ * @param {string} url
+ * @param {function} callback
+ * @returns {Promise.<TResult>}
+ */
+export default function getJSON(url,callback=undefined){
+    return fetch(url).then((response)=> response.json()).then((res)=>{
+        return callback === undefined
+            ? res
+            : typeof callback === "function"
+                ? callback(res)
+                : ()=>{throw "Callback must be a function"}
+            ;
+    });
 }
