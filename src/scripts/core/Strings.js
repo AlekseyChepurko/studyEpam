eb05ba680e4d1926bfff94f61d43783725167528
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-06-06 03:37:58 +0300

    Camalize function added

---
+++ src/scripts/core/Strings.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Алексей on 06.06.2017.
+ */
+
+export function camalize(str) {
+    return str
+        .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+        .replace(/\s/g, '')
+        .replace(/^(.)/, function($1) { return $1.toLowerCase(); });
+}
