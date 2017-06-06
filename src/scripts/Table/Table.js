378c51a8f7558917b144827aeb88b2c7925f98d7
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-06-06 03:38:32 +0300

    Table rendering to the documentFragment refactoring

--- src/scripts/Table/Table.js
+++ src/scripts/Table/Table.js
@@ -5,38 +5,87 @@
 import Sugard,{Component} from '../core/Component'
 
 export default class Table extends Component{
-    constructor(props, type='table') {
-        super(type, props);
+    constructor(props,) {
+        super('fragment', props);
         this.state = {
             title: this.props.data.title,
             header: this.props.data.header,
             body: this.props.data.body
         };
     }
-    componentWillRender(){}
-    componentDidRender(){}
-
     render(){
-        return(
-            new Head({
+        return([
+            new Header({
+                data: this.state.title
+            }),
+            new Body({
+                header: this.state.header,
+                body: this.state.body
+            })
+        ]);
+    }
+}
+class Header extends Component {
+    constructor(props) {
+        super('header', props);
+    }
+    render(){}
+}
+class Body extends Component{
+    constructor(props) {
+        super('table', props)
+    }
+    render(){
+        return ([
+            new tHead({
                 attributes: {
                     class: "headerClass"
                 },
-                data: ['a','b','c','d']
+                data: this.props.header
+            }),
+            new tBody({
+                data: this.props.body
             })
-        );
+        ])
     }
 }
-
-class Head extends Component{
-    constructor(props={}, type='thead') {
-        super(type, props);
+class tHead extends Component{
+    constructor(props) {
+        super('thead', props);
     }
     render(){
         const line = new Component('tr');
+        const underLine = new Component('tr');
         line.addChildren(this.props.data.map((item) => {
-            return new Component('th', {data: item});
+            return new Component('th', {
+                data: item,
+                attributes: {
+                    align: "left",
+                    class: `table__head_item`
+                }
+            });
         }));
-        return line;
+        underLine.addChildren(this.props.data.map(() => {
+            return new Component('th', {
+                attributes: {
+                    class: `underline`
+                }
+            });
+        }));
+        return [line, underLine];
     };
+}
+class tBody extends Component{
+    constructor(props){
+        super('tbody', props)
+    }
+    render(){
+        const lines = this.props.data.map((item)=>{
+            const line = new Component('tr');
+            line.addChildren(Object.values(item).map(data => new Component('td', {data: data})));
+            return line;
+        });
+        return lines;
+
+    }
 }
