30365e294802430d375481898b54837d6ab946db
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-06-15 01:33:41 +0300

    moved to node_modules dir

--- src/scripts/core/Selections.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Алексей on 02.06.2017.
- */
-
-
-export const selectFirst = (selector, element=document)=> element.querySelector(selector);
-export const select = (selector, element=document)=> element.querySelectorAll(selector);
--- src/scripts/core/test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Алексей on 04.06.2017.
- */
-
-export default function test(testName,res,expected){
-    if (res === expected)
-        return console.log(`%c OK! - ${testName}`, 'color: green;');
-    return console.log(`%c Failure - ${testName}: Expected ${expected} given ${res}`, 'color: red; font-weight: bold;');
-}
