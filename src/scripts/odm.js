7c92b5442c68e7e6249fd4c603470fb90898ea49
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-06-15 04:22:48 +0300

    CommonInit script now imports from ./common.js

--- src/scripts/odm.js
+++ src/scripts/odm.js
@@ -1,98 +1,14 @@
 /**
- * Created by Алексей on 14.06.2017.
- */
-/**
  * Created by Алексей on 18.03.2017.
  */
-import {classes} from './core/classes'
-import {selectFirst, select} from './core/Selections'
-import {sub_elements} from './SubElements'
-import {openTab} from './openTab'
-import Table from './Table/Table'
-import Sugard, {render} from './core/Component'
-import getJSON from './core/Async'
-import tests from './core/Component.test'
-// tests();
+import testData from 'Root/testData/odmData.json'
+import {classes} from 'Vendor/core/classes'
+import {selectFirst} from 'Vendor/core/Selections'
+import CommonInit from './common'
+import style from 'Styles/odm.scss'
 
-getJSON('/data/odmData.json',(tables)=>{
-    tables.forEach((table)=>{
-        let res = new Table({
-            data:{
-                title: table.title,
-                header: table.header,
-                body: table.body,
-            },
-            attributes: {
-                class: "ololo"
-            }
-        });
-        render(res, selectFirst('.tabcontent#approval'));
-    });
+CommonInit(testData, 'approval');
+
+selectFirst("#nav_menu__togler").addEventListener('click', e => {
+    classes.toggle(e.target.parentElement, "active");
 });
-
-select("nav.tab>button").forEach((tab) => {
-    tab.addEventListener('click', (e)=>{
-        e.preventDefault();
-        openTab(e, e.target.getAttribute("for"))
-    })
-});
-
-selectFirst("#defaultOpen").click();
-
-select(".to_show").forEach(item => {
-    item.addEventListener('click', (e)=> {
-        sub_elements.open(e.currentTarget);
-    })
-});
-
-select(".btn__close_subElement").forEach(item=>{
-    item.addEventListener('click', e=>{
-        sub_elements.close(e.currentTarget);
-    })
-});
-
-selectFirst("#fullscreen").addEventListener('click', (e)=>{
-    const elem = e.target;
-    const html = selectFirst("html");
-    if (elem.requestFullscreen) {
-        html.requestFullscreen();
-    } else if (elem.msRequestFullscreen) {
-        html.msRequestFullscreen();
-    } else if (elem.mozRequestFullScreen) {
-        html.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) {
-        html.webkitRequestFullscreen();
-    }
-    // console.log("hi");
-    // const e = new Event("keydown");
-    // e.key="F11";
-    // e.keyCode=122;
-    // e.which=e.keyCode;
-    // e.altKey=false;
-    // e.ctrlKey=true;
-    // e.shiftKey=false;
-    // e.metaKey=false;
-    // // e.bubbles=true;
-    // console.log(document.dispatchEvent(e));
-    // // console.log(e);
-}) ;
-
-selectFirst(".adaptive_menu__toogler").addEventListener('click', e => {
-    classes.toggle(e.target, "active");
-    classes.toggle(selectFirst(".adaptive_menu__wrap"), "active");
-});
-
-select(".dropdown").forEach(item => {
-    item.addEventListener('click', e=>{
-        classes.toggle(e.currentTarget, "active");
-    })
-});
-
-try {
-    selectFirst("#nav_menu__togler").addEventListener('click', e => {
-        classes.toggle(e.target.parentElement, "active");
-    })}
-catch (e){
-    console.log("error in event listener");
-}
-
